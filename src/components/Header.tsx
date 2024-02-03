9a85e6453e0dc615b72b55125a2d8163a7ca60b6
Jérémy Gendre <jeremy@atipik.ch>
2024-02-03 11:46:54 +0100

    feat: change main icon and improve header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,5 @@
-import { useLocation } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import HeaderLink from "./HeaderLink";
-import HomeIcon from "./icons/HomeIcon";
 import LearnIcon from "./icons/LearnIcon";
 import ListCheckIcon from "./icons/ListCheckIcon";
 
@@ -8,10 +7,17 @@ export default function Header() {
   const { pathname } = useLocation();
   return (
     <header className="sticky top-0 z-50">
-      <div className="w-full bg-gray-200 shadow-lg text-center text-sm px-3 py-2 flex flex-wrap gap-1">
-        <HeaderLink to="/" label="Accueil" icon={<HomeIcon className="text-base" />} active={pathname === '/'}/>
-        <HeaderLink to="/learn" label="Learn" icon={<LearnIcon className="text-base" />} active={pathname.includes('/learn')}/>
-        <HeaderLink to="/quizz" label="Quizz" icon={<ListCheckIcon className="text-base" />} active={pathname.includes('/quizz')}/>
+      <div className="w-full bg-gray-200 shadow-lg text-center text-sm px-3 py-2 flex flex-wrap gap-2 xs:gap-4 sm:gap-6 md:gap-8">
+        <Link to="/" className="flex gap-2 items-center font-bold text-xl text-sky-700">
+          <img src="/images/earth-icon.png" alt="Logo" className="w-8 h-8" />
+          <div className="hidden sm:block">
+            Capital Cities
+          </div>
+        </Link>
+        <div className="flex flex-wrap gap-2 flex-1 justify-end sm:justify-normal">
+          <HeaderLink to="/learn" label="Learn" icon={<LearnIcon className="text-base" />} active={pathname.includes('/learn')}/>
+          <HeaderLink to="/quizz" label="Quizz" icon={<ListCheckIcon className="text-base" />} active={pathname.includes('/quizz')}/>
+        </div>
       </div>
     </header>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,14 @@ module.exports = {
         'pos-100': '100% 100%',
       },
     },
+    screens:{
+      'xs': '450px',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    }
   },
   plugins: [],
 }
