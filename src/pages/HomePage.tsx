b91bb3652595ccff6ea433abe43809537cb6631d
Jérémy Gendre <jeremy@atipik.ch>
2025-04-19 19:22:53 +0200

    fix: home page link size on mobile

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,7 +2,7 @@ import HomeLink from "../components/HomeLink";
 
 export default function HomePage(){
   return (
-    <div className="flex w-full justify-center items-center h-full gap-3 flex-wrap">
+    <div className="flex flex-col md:flex-row w-full justify-center items-stretch md:items-center h-full gap-3 flex-wrap">
       <HomeLink label="Capitales" to="/capital-cities"/>
       <HomeLink label="Empire Romain" to="/roman-empire"/>
     </div>
